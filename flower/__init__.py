39a64c12451facddc5eac8efa055e57fdf86b559
sleepwalker265 <nguyenlamanh2652005@gmail.com>
2026-06-16 07:48:29 +0700

    Update imports to include EpisodicProtoNet
    Replaced ProtoNet with EpisodicProtoNet in imports and __all__.

--- flower/__init__.py
+++ flower/__init__.py
@@ -1,21 +1,17 @@
 """
 Flower - Federated ProtoNet Package
-
-A federated learning implementation of Prototypical Networks using Flower framework.
 """
-
 __version__ = "0.1.0"
 
-from .models import ProtoNet, build_model, get_parameters, set_parameters
+from .models import EpisodicProtoNet, build_model, get_parameters, set_parameters
 from .flower_client import ProtoNetClient
-from .data_utils import load_episodic_data, split_support_query
+from .data_utils import get_dataloaders
 
 __all__ = [
-    "ProtoNet",
+    "EpisodicProtoNet",
     "build_model",
     "get_parameters",
     "set_parameters",
     "ProtoNetClient",
-    "load_episodic_data",
-    "split_support_query",
+    "get_dataloaders",
 ]
